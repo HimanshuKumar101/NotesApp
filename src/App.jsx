2c7fccf64c5dab7a028f9a14206e3b26f3fa95b5
himanshu101 <himanshukumar.connect@gmail.com>
2025-02-18 00:11:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="p-10 bg-gray-200">
+      <p className="text-red-500 text-3xl font-bold">Tailwind is Working! 🎉</p>
+      <p className="text-blue-600 text-lg">This is blue text.</p>
+      <p className="text-green-500 text-xl">This is green text.</p>
+    </div>
+  );
+};
+
+export default App;
