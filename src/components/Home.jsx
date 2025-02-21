c0a77a49376cc891f42001b9e0a12c2f673e9a24
himanshu101 <himanshukumar.connect@gmail.com>
2025-02-21 20:40:19 +0530

    home page create

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,85 +1,98 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
-import { addToPastes, updateToPastes } from '../redux/pasteSlice';
+import React, { useEffect, useState } from "react";
+import { copy, PlusCircle } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { addToPastes, updateToPastes } from "../redux/pasteSlice";
+import toast from "react-hot-toast";
 
 const Home = () => {
+  const [title, setTitle] = useState("");
 
-  const [title, setTitle] = useState('');
+  const [value, setValue] = useState("");
 
-  const[value, setValue] = useState("");
-
-  const[searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const pasteId = searchParams.get("pasteId");
-  
+
   const dispatch = useDispatch();
 
+  const allPastes = useSelector((state) => state.paste.pastes);
 
+  const createPaste = () => {
+    const paste = {
+      title: title,
+      content: value,
+      _id:
+        pasteId ||
+        Date.now().toString(36) + Math.random().toString(36).substring(2),
+      createdAt: new Date().toIS0String(),
+    };
 
-  function createPaste(){
-       const paste = {
-        title: title,
-        content: value,
-        _id: pasteId || Date.now().toString,
-         createAt: new Date().toISOString(),
+    if (pasteId) {
+      //update
+      dispatch(updateToPastes(paste));
+    } else {
+      //create
+      dispatch(addToPastes(paste));
+    }
 
-       }
-       if(pasteId){
-        //update
-        dispatch(updateToPastes);
-       }else{
+    //after creation or update
+    setTitle("");
+    setValue("");
 
-        //create
-        dispatch(addToPastes);
+    //remove the pasteId from the url, after creating / updating a paste
+    setSearchParams({});
+  };
 
-       }
-   
-       //after creation or 
-       setTitle('');
-       setValue('');
-       setSearchParams({});
+  const resetPaste = () => {
+    setTitle("");
+    setValue("");
+    setSearchParams({});
+    //navigate("/")
+  };
 
-  }
-
+  useEffect(() => {
+    if (pasteId) {
+      const paste = paste.find((p) => p._id === pasteId);
+      if (paste) {
+        setTitle(paste.title);
+        setValue(paste.content);
+      }
+    }
+  }, [pasteId, pastes]);
 
   return (
-   <div>
-     <div className='flex flex-row gap-7
-     place-content-between'>
-        <input
-        className=' p-1 rounded-2xl mt-2
-        w-[66%] pl-4'
-        type='text'
-        placeholder='enter title here'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        >
+    <div className="w-full h-full py-10 max-w-[1200px] mx-auto px-5 lg:px-0">
+      <div className="flex flex-col gap-y-5 items-start">
+        <div className="w-full flex flex-row gap-x-4 justify-between items-center">
+          <input
+            className={`${ pasteId ? "w-[80%]" : "w-[85%]" } text-black border border-input rounded-md p-2`}
+            type="text"
+            placeholder="enter title here"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+
+          <button onClick={createPaste} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue 
+          font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600
+          dark:hover:bg-blue-700">
+            {pasteId ? "Update My Paste" : "Create My Paste"}
+          </button>
           
-        </input>
-
-        <button 
-        onClick={createPaste}
-        className='p-2 rounded-2xl mt-2 '>  
-          {
-            pasteId ? "Update My Paste" : "Create My Paste"
-          }
-        </button>
+        </div>
+        <div className="mt-8">
+          <textarea
+            className="rounded-2xl mt-4,
+      min-w-[500px] p-4"
+            value={value}
+            placeholder="enter content here"
+            onChange={(e) => setValue(e.target.value)}
+            rows={20}
+          />
+        </div>
+      </div>
     </div>
-    <div className='mt-8'>
-      <textarea 
-      className='rounded-2xl mt-4,
-      min-w-[500px] p-4'
+  );
+};
 
-        value ={value}
-        placeholder="enter content here"
-        onChange={(e) => setValue(e.target.value)}
-        rows={20}
-      />
-
-    </div>
-   </div>
-  )
-}
-
-export default Home
+export default Home;
--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { act } from 'react';
 import toast from 'react-hot-toast';
 
 
@@ -17,18 +18,59 @@ export const pasteSlice = createSlice({
           const paste = action.payload;
 
           //add a check -> paste already exist wala case
+
+          
+
+
           
           state.pastes.push(paste);
           localStorage.setItem("pastes", JSON.stringify(state.pastes));
           toast("Paste Created Successfully")
     },
     updateToPastes: (state,action) => {
+
+      const paste = action.payload;
+      const index = state.pastes.findIndex((item) => item.id === paste.id);
+
+      if( index >= 0){
+        state.pastes[index] = paste;
+
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+        toast.success("Paste Updated");
+      }
+
+
+
       
     },
     resetAllPastes: (state, action) => {
+
+
+      state.pastes = [];
+
+      localStorage.removeItem("pastes");
       
     },
     removeFromPastes: (state, action) => {
+
+      const pasteId = action.payload;
+      
+      console.log(pasteId);
+
+      const index = state.pastes.findIndex((item) => item._id === pasteId);
+
+      if( index >= 0){
+        
+        state.pastes.splice(index, 1);
+
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+
+        toast.success("Paste Deleted");
+
+        
+      }
+
  
     },
   },
